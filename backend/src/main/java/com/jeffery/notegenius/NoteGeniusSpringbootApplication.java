6f2d0ecb1fc485d82d3bbdc2b522ba8bfee2835a
Jeffery Liu <a4264428@gmail.com>
2025-06-25 16:17:06 +0800

    add backend template

---
+++ backend/src/main/java/com/jeffery/notegenius/NoteGeniusSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jeffery.notegenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteGeniusSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteGeniusSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeffery/notegenius/NoteGeniusSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffery.notegenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteGeniusSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
